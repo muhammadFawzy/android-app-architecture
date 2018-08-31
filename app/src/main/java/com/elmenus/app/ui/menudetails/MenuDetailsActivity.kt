e286e0fa602047ecd6b48fb507cf84ebfc3d72a8
Mohamed Fawzy <mfawzi109@gmail.com>
2018-08-31 11:36:48 +0200

    Fix details activity

--- app/src/main/java/com/elmenus/app/ui/menudetails/MenuDetailsActivity.kt
+++ app/src/main/java/com/elmenus/app/ui/menudetails/MenuDetailsActivity.kt
@@ -1,22 +1,31 @@
 package com.elmenus.app.ui.menudetails
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v7.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.elmenus.app.R
+import com.elmenus.app.databinding.ActivityMenuDetailsBinding
 import com.elmenus.app.model.Menu
-import kotlinx.android.synthetic.main.activity_menu_details.*
 
 class MenuDetailsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMenuDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_menu_details)
+        initData()
 
-        var menu=intent.extras?.getSerializable("item") as Menu
 
-        Glide.with(this)
-                .load(menu.photoUrl)
-                .into(image)
+    }
+
+    private fun initData() {
+        var menu = intent.extras?.getSerializable("item") as Menu
+        binding.model = menu
+        var collapsingToolbarLayout = findViewById<CollapsingToolbarLayout>(
+                R.id.collapsing_toolbar)
+        collapsingToolbarLayout.title = menu.name
+        collapsingToolbarLayout.setExpandedTitleColor(
+                resources.getColor(android.R.color.transparent))
     }
 }

797cd4d1dc099bee159d661292b9aa8d927e492f
Mohamed Fawzy <mfawzi109@gmail.com>
2018-08-31 08:33:33 +0200

    Add model pacakge with menu model

---
+++ app/src/main/java/com/elmenus/app/model/Menu.kt
@@ -0,0 +1,14 @@
+package com.elmenus.app.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MenuList(@SerializedName("items") val items: List<Menu>) {
+
+}
+
+data class Menu(@SerializedName("id") val id: Int
+                , @SerializedName("name") val name: String
+                , @SerializedName("photoUrl") val photoUrl: String
+                , @SerializedName("description") val description: String) {
+}
+
--- app/src/main/java/com/elmenus/app/ui/menudetails/MenuDetailsActivity.kt
+++ app/src/main/java/com/elmenus/app/ui/menudetails/MenuDetailsActivity.kt
@@ -1,7 +1,7 @@
 package com.elmenus.app.ui.menudetails
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.elmenus.app.R
 
 class MenuDetailsActivity : AppCompatActivity() {

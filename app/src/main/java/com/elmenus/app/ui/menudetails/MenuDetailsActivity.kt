2d48cd814f175ba0c5e73409c223d31887868e12
Mohamed Fawzy <mfawzi109@gmail.com>
2018-08-31 08:27:46 +0200

    Add details activity

---
+++ app/src/main/java/com/elmenus/app/ui/menudetails/MenuDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.elmenus.app.ui.menudetails
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.elmenus.app.R
+
+class MenuDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_details)
+    }
+}

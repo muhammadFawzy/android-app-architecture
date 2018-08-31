7702d91ebdeba73ee47b4b15c8c7c9fc9380743d
Mohamed Fawzy <mfawzi109@gmail.com>
2018-08-31 09:54:59 +0200

    Add viewmodel

--- app/src/main/java/com/elmenus/app/ui/menulist/MenuViewModel.kt
+++ app/src/main/java/com/elmenus/app/ui/menulist/MenuViewModel.kt
@@ -1,6 +1,13 @@
 package com.elmenus.app.ui.menulist
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableBoolean
+import android.databinding.ObservableInt
+import android.view.View
 
 class MenuViewModel : ViewModel() {
+
+    var loadMore = ObservableInt(View.GONE)
+    var loading = ObservableBoolean(false)
+
 }
--- app/src/main/java/com/elmenus/app/ui/menulist/MenusListActivity.kt
+++ app/src/main/java/com/elmenus/app/ui/menulist/MenusListActivity.kt
@@ -1,6 +1,8 @@
 package com.elmenus.app.ui.menulist
 
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
@@ -9,6 +11,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout.VERTICAL
 import com.elmenus.app.R
+import com.elmenus.app.databinding.ActivityMenusListBinding
 import com.elmenus.app.model.Menu
 import com.elmenus.app.ui.menudetails.MenuDetailsActivity
 import kotlinx.android.synthetic.main.activity_menus_list.*
@@ -17,16 +20,20 @@ import kotlinx.android.synthetic.main.activity_menus_list.*
 class MenusListActivity : AppCompatActivity(), ItemClickListener {
 
     lateinit var adapter: MenuAdapter
+    lateinit var binding: ActivityMenusListBinding
+    lateinit var viewmModel: MenuViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menus_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_menus_list)
         initData()
 
 
     }
 
     private fun initData() {
+        viewmModel = ViewModelProviders.of(this).get(MenuViewModel::class.java)
+        binding.viewmodel = viewmModel
         adapter = MenuAdapter(this)
         recycler_menus.layoutManager = LinearLayoutManager(this, VERTICAL, false)
         recycler_menus.adapter = adapter

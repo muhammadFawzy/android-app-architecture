230b7b4caa81295ad0b573fee8f754f27a9a7a0c
Mohamed Fawzy <mfawzi109@gmail.com>
2019-06-14 13:01:01 +0200

    delete unused dependencies

--- app/src/main/java/com/example/app/domain/entity/MenuEntity.kt
+++ app/src/main/java/com/example/app/domain/entity/MenuEntity.kt
@@ -1,15 +1,12 @@
 package com.example.app.domain.entity
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class MenuList(@SerializedName("items") val items: List<Menu>)
 
-@Entity
 data class Menu(@SerializedName("id") val id: Int
-                , @PrimaryKey @SerializedName("name") val name: String
+                , @SerializedName("name") val name: String
                 , @SerializedName("photoUrl") val photoUrl: String
                 , @SerializedName("description") val description: String) : Serializable
 
